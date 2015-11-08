aaf177b1d5082330e29c521289950e31f7e0e4cb
dijalmasilva <dijalmacz@gmail.com>
2015-11-08 13:59:03 -0300

    Falta upload da imagem

--- src/main/java/servlets/ControleCadastroUsuario.java
+++ src/main/java/servlets/ControleCadastroUsuario.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -27,21 +28,23 @@ public class ControleCadastroUsuario extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         req.setCharacterEncoding("UTF-8");
-        
-        String nomeCompleto = req.getParameter("nomeCompleto");
-        String apelido = req.getParameter("apelido");
-        String senha = req.getParameter("senhaCadastro");
-        String email = req.getParameter("emailCadastro");
-        String data = req.getParameter("dataDeNascimento");
-        String estado = req.getParameter("estado");
-        String cidade = req.getParameter("cidade");
-        String foto = "imagens/user.png";
 
-        Usuario u = new Usuario(email, senha, nomeCompleto, apelido, Date.valueOf(data), cidade, estado, true, foto);
-        boolean cadastrou = new GerenciadorUsuario().adicionar(u);
+        JOptionPane.showMessageDialog(null, req.getParameter("imagem"));
         
-        req.setAttribute("cadastrou", cadastrou);
-        req.setAttribute("emailCadastro", email);
+//        String nomeCompleto = req.getParameter("nomeCompleto");
+//        String apelido = req.getParameter("apelido");
+//        String senha = req.getParameter("senhaCadastro");
+//        String email = req.getParameter("emailCadastro");
+//        String data = req.getParameter("dataDeNascimento");
+//        String estado = req.getParameter("estado");
+//        String cidade = req.getParameter("cidade");
+//        String foto = "imagens/user.png";
+//
+//        Usuario u = new Usuario(email, senha, nomeCompleto, apelido, Date.valueOf(data), cidade, estado, true, foto);
+//        boolean cadastrou = new GerenciadorUsuario().adicionar(u);
+//        
+//        req.setAttribute("cadastrou", cadastrou);
+//        req.setAttribute("emailCadastro", email);
 
         req.getRequestDispatcher("cadastro.jsp").forward(req, resp);
     }
